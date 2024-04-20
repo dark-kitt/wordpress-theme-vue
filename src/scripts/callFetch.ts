ec1953c4a635872dce9447ba7321b69a39cc2c78
dark-kitt <42038118+dark-kitt@users.noreply.github.com>
2024-04-20 19:54:57 +0200

    feat(scripts): + optional headers in callFetch function

--- src/scripts/callFetch.ts
+++ src/scripts/callFetch.ts
@@ -1,18 +1,29 @@
+/**
+ * JS fetch() helper function
+ *
+ * @param method POST, GET, PUT, and DELETE
+ * @param token authentication for REST-API
+ * @param endpoint location of data
+ * @param headers object of headers
+ */
 export default <T>({
   method = 'POST',
-  token,
-  endpoint
+  headers = null,
+  endpoint,
+  token
 }: {
   method: string;
   token?: string;
   endpoint: string;
+  headers?: { [key: string]: string };
 }): Promise<T> => {
   return fetch(`${process.env.REST_API}/wp-json/${process.env.REST_NAMESPACE}/${endpoint}`, {
     method,
     headers: {
       ...(token && { Authorization: `Bearer ${token}` }),
       'Content-Type': 'application/json'
-    }
+    },
+    ...(headers && headers)
   }).then(async response => {
     const data = (await response.json()) as Promise<T>;
 
